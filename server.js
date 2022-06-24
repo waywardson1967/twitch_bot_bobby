d04b633b09053de661a9293156beba27e7315213
waywardson1967 <107893004+waywardson1967@users.noreply.github.com>
2022-06-23 21:28:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ client.on('message', (channel, tags, message, self) => {
         
 	
     if (command === 'join'){
-	    user = tags.username.toString().replace(/[^a-zA-Z0-9 ]/g, '');
+	    user = tags.username.toString();
 	    UserList.push(user.replace(/[^a-zA-Z0-9 ]/g, ''));
 	    client.say(channel, `${tags.username} joined the queue!`);
 	    numOfPeopleInList++;
