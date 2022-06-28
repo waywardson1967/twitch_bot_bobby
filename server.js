f624751754634367b0ae665965f42cb2e1d20b65
waywardson1967 <107893004+waywardson1967@users.noreply.github.com>
2022-06-28 17:49:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ client.on('message', (channel, tags, message, self) => {
     
     const [raw, command, argument] = message.match(regexpCommand);
 	client.say(channel, "got here 3");
-	te = tags.badges.braodcaster;
+	te = tags.badges.broadcaster;
 	client.say(channel, te.toString());
     
     if (command === 'join'){
