4681c89feb8e6c601fd38bda690556580fb0f956
waywardson1967 <107893004+waywardson1967@users.noreply.github.com>
2022-07-04 17:41:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -89,8 +89,9 @@ client.on('message', (channel, tags, message, self) => {
 			UserList.push(player);
 			client.say(channel, `${tags.username} joined the queue and you're up next!`);
 		} else{
-			client.say(channel,"testing");
+			
 			for (let i = 4; i < UserList.length; i++){
+				client.say(channel,"testing");
 				if (player.username === UserList[i].username){
 					UserList.splice(i, 0, player);
 					estPlayerTimer = i * 20;
