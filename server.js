57ae5d7a943a757a8f1eda00c5eb26926767ff65
waywardson1967 <107893004+waywardson1967@users.noreply.github.com>
2022-07-04 22:17:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -60,8 +60,9 @@ client.on('message', (channel, tags, message, self) => {
 		AlreadyJoined = 0;
 		if (command === 'add'){
 			if (tags.badges.hasOwnProperty('moderator') || tags.badges.hasOwnProperty('broadcaster')) {
+				client.say(channel,`the argument is ${argument}`);
 				user = argument.toString();
-				if (argument === ""){
+				if (user === ""){
 					client.say(channel, "Silly mod, you need to say WHO you want to add.");
 					return;
 				}
