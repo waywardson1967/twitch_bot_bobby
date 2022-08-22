adf33b223d73ac3e6cc253956ba3e0951991e573
waywardson1967 <107893004+waywardson1967@users.noreply.github.com>
2022-08-22 14:24:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -708,7 +708,7 @@ client.on('message', (channel, tags, message, self) => {
 	} 
 });
 
-process.on('uncaughtException', err => {
+client.on('uncaughtException', err => {
   console.error('There was an uncaught error', err);
   //process.exit(1); // mandatory (as per the Node.js docs)
 });
