79972502bf1bb11e205f6b2124bfee4536c18ac0
waywardson1967 <107893004+waywardson1967@users.noreply.github.com>
2023-01-17 18:08:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ let streamerIsLive = 1;
 let prevStreamerIsLive = 1;
 function checkStreamInfo(data) {
     //console.log("got here");
-	console.log(data);
+	console.log(data.data[0]);
 	//let streamerLocationInList = 0;
 	if(data.hasOwnProperty('data')){ //potentially live
 		console.log("that worked");
